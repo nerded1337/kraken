c5d9fecb63b5572525cfdd3546b7a0ddab42c20e
Chris Mahon <cnmahon@gmail.com>
2016-03-09 01:21:31 +1100

    First private API call (balance) working

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,10 @@ module Main where
 
 import Control.Monad
 import Control.Monad.IO.Class
+import qualified Data.ByteString.Char8 as BC (pack)
+import Data.Default
 import Data.Text
+import System.Environment
 
 import Kraken.Types
 import Kraken.Rest
@@ -10,11 +13,17 @@ import Kraken.Rest
 -----------------------------------------------------------------------------
 
 main :: IO ()
-main = void $ runKraken $ do
-  t <- time
-  io t
-  a <- assets (AssetsOptions Currency [XXBT,XETH])
-  io a
+main = do
+  Just apiKey <- lookupEnv "KRAKEN_API_KEY"
+  Just apiPrivKey <- lookupEnv "KRAKEN_API_PRIVKEY"
+  apiPass <- lookupEnv "KRAKEN_API_PASSWORD"
+  let Right cfg = mkConfig (BC.pack apiKey)
+                           (BC.pack apiPrivKey)
+                           (fmap BC.pack apiPass)
+  void $ runKraken cfg $ do
+    io =<< time
+    io =<< assets (AssetsOptions Currency [XXBT,XETH])
+    io =<< balance
  where
   io :: Show a => a -> KrakenT ()
   io = liftIO . print
--- src/Kraken/Rest.hs
+++ src/Kraken/Rest.hs
@@ -1,7 +1,24 @@
 module Kraken.Rest where
 
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Either
+import           Control.Monad.Trans.Reader
+import           Crypto.Hash
+import           Data.Aeson.Types
+import           Data.Byteable
+import           Data.ByteString
+import qualified Data.ByteString.Base64 as B64
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as BL
+import           Data.Default
+import           Data.Monoid
 import           Data.Proxy
+import           Data.Text (Text)
+import qualified Data.Text as T (pack)
+import           Data.Text.Encoding (decodeUtf8)
+import           Data.Time
+import           Data.Time.Clock.POSIX
 import           Servant.API
 import           Servant.Client
 
@@ -17,28 +34,86 @@ restPort = 443
 
 -----------------------------------------------------------------------------
 
-type KrakenT = EitherT ServantError IO
+data Config = Config
+  { configAPIKey     :: ByteString
+  , configPrivateKey :: ByteString
+  , configPassword   :: Maybe ByteString
+  }
+
+instance Default Config where
+  def = Config
+    { configAPIKey     = ""
+    , configPrivateKey = ""
+    , configPassword   = Nothing
+    }
+
+mkConfig :: ByteString -> ByteString -> Maybe ByteString -> Either String Config
+mkConfig ak pk pw = case B64.decode pk of
+  Right pkd -> Right $ Config ak pkd pw
+  Left  e   -> Left e
 
 -----------------------------------------------------------------------------
 
-runKraken :: KrakenT a -> IO (Either ServantError a)
-runKraken = runEitherT
+type ServantT = EitherT ServantError IO
+type KrakenT  = ReaderT Config ServantT
+
+runKraken :: Config -> KrakenT a -> IO (Either ServantError a)
+runKraken cfg = runEitherT . flip runReaderT cfg
 
 -----------------------------------------------------------------------------
 
-type KrakenAPI     = APIVersion :> Services
-type APIVersion    = "0"
-type Public        = "public"
-type Services      = TimeService
-                     :<|>
-                     AssetsService
-type TimeService   = Public
-                     :> "Time"
-                     :> Get '[JSON] Time
-type AssetsService = Public
-                     :> "Assets"
-                     :> ReqBody '[FormUrlEncoded] AssetsOptions
-                     :> Post '[JSON] Assets
+type KrakenAPI          = TimeService
+                          :<|>
+                          AssetsService
+                          :<|>
+                          AccBalService
+type TimeService        = APIVersion
+                          :> Public
+                          :> "Time"
+                          :> Get '[JSON] Time
+type AssetsService      = APIVersion
+                          :> Public
+                          :> "Assets"
+                          :> ReqBody '[FormUrlEncoded] AssetsOptions
+                          :> Post '[JSON] Assets
+type AccBalService      = APIVersion
+                          :> Private
+                          :> "Balance"
+                          :> Header "API-Key" Text
+                          :> Header "API-Sign" Text
+                          :> ReqBody '[FormUrlEncoded] (PrivReq ())
+                          :> Post '[JSON] Value
+type APIVersion         = "0"
+type Public             = "public"
+type Private            = "private"
+
+{-
+type KrakenAPI          = APIVersion :> Services
+type PrivateRequest a b = Header "API-Key" Text
+                          :> Header "API-Sign" Text
+                          :> PublicRequest a b
+type PublicRequest a b  = ReqBody '[FormUrlEncoded] a
+                          :> Post '[JSON] b
+-}
+
+-----------------------------------------------------------------------------
+
+instance ToFormUrlEncoded () where
+  toFormUrlEncoded () = []
+
+data PrivReq a = PrivReq
+  { privreqNonce :: Int
+  , privreqOTP   :: Maybe ByteString
+  , privreqData  :: a
+  }
+
+instance (ToFormUrlEncoded a) => ToFormUrlEncoded (PrivReq a) where
+  toFormUrlEncoded PrivReq{..} = 
+    [ ("nonce",T.pack . show $ privreqNonce) ]
+    ++
+    [ ("otp"  ,decodeUtf8 otp) | Just otp <- [privreqOTP] ]
+    ++
+    toFormUrlEncoded privreqData
 
 -----------------------------------------------------------------------------
 
@@ -47,7 +122,37 @@ api = Proxy
 
 -----------------------------------------------------------------------------
 
-time       :: KrakenT Time
-assets     :: AssetsOptions -> KrakenT Assets
+time_      :: ServantT Time
+assets_    :: AssetsOptions -> ServantT Assets
+balance_   :: Maybe Text -> Maybe Text -> PrivReq () -> ServantT Value
 
-time :<|> assets = client api (BaseUrl Https restHost restPort)
+time_
+  :<|> assets_
+  :<|> balance_ = client api (BaseUrl Https restHost restPort)
+
+-----------------------------------------------------------------------------
+
+time :: KrakenT Time
+time = lift time_
+
+assets :: AssetsOptions -> KrakenT Assets
+assets = lift . assets_
+
+balance :: KrakenT Value
+balance = do
+  Config{..} <- ask
+  utcTime <- liftIO getCurrentTime
+  let apiKey       = decodeUtf8 configAPIKey
+      apiSign      = decodeUtf8 . B64.encode . toBytes $ hmacMsg
+      msg          = uri <> hashPostData
+      hmacMsg      = hmac configPrivateKey msg :: HMAC SHA512
+      privReq      = PrivReq nonce configPassword ()
+      postData     = BL.toStrict $ mimeRender (Proxy :: Proxy FormUrlEncoded)
+                                              privReq
+      hashPostData = toBytes (hash (nonceBytes <> postData) :: Digest SHA256)
+      nonce        = fromEnum . utcTimeToPOSIXSeconds $ utcTime
+      nonceBytes   = BC.pack . show $ nonce
+      uri          = BC.pack $ "/" ++ (show $ safeLink api
+                                               (Proxy :: Proxy AccBalService))
+  lift $ balance_ (Just apiKey) (Just apiSign) privReq
+
